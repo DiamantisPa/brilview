819ceb30a0937e3661ea5180a195bdd4f05e35d9
Zhen Xie <zhen@pczhen.cern.ch>
2016-11-18 19:25:30 +0100

    added

--- brilview/cmmdhandler.py
+++ brilview/cmmdhandler.py
@@ -1,3 +1,23 @@
+import os
+import sys
+import subprocess
 
-def brilcalcHandler(cmmargs):
-    
+def brilcalcLumiHandler(brilcalcargs,cmmd=None):
+    '''
+    brilcalcargs: arguments for brilcalc lumi 
+    cmmd : for passing brilcalc command in case you don't run standard brilcalc
+    '''
+    if not brilcalcargs:
+        print 'about to return empty'
+        return ''
+    args = []
+    if cmmd:
+        args.append(cmmd)
+    else:
+        args.append('brilcalc')
+    args = args+['lumi']+brilcalcargs+['--output-style','csv','--without-checkjson']
+    result = subprocess.check_output(args,stderr=subprocess.STDOUT)
+    return '\n'.join([i for i in result.split('\n') if  len(i) > 0])
+if __name__=='__main__':
+    print brilcalcLumiHandler(['-r','284077'],cmmd='/home/zhen/work/brilws/brilcalc-run.py')
+        
