bf7eda6a1485853469f734b6bd8268dedbad18f7
Zhen Xie <zhen@pczhen.cern.ch>
2016-11-18 21:10:48 +0100

    cmmd to list

--- brilview.py
+++ brilview.py
@@ -1,7 +1,7 @@
 import flask
 import json
-from handlers import brilcalc
-
+#from handlers import brilcalc
+from brilview import cmmdhandler
 
 app = flask.Flask(__name__)
 
@@ -18,7 +18,7 @@ def forward_to_server():
         return ('Bad request. Query body must be not empty.', 400)
     run_from = data['from']
     run_to = data['to']
-    result = brilcalc.get(run_from, run_to)
+    result = brilcalcLumiHandler(['--begin',str(run_from),'--end',str(run_to)] , cmmd=['/afs/cern.ch/cms/lumi/brilconda-1.1.7/bin/python','/home/data/brilws/brilcalc-run.py'])
     return flask.Response(json.dumps(result), mimetype='application/json')
 
 
--- brilview/cmmdhandler.py
+++ brilview/cmmdhandler.py
@@ -2,22 +2,23 @@ import os
 import sys
 import subprocess
 
-def brilcalcLumiHandler(brilcalcargs,cmmd=None):
+def brilcalcLumiHandler(brilcalcargs,cmmd=[]):
     '''
     brilcalcargs: arguments for brilcalc lumi 
     cmmd : for passing brilcalc command in case you don't run standard brilcalc
     '''
+    print cmmd
     if not brilcalcargs:
         print 'about to return empty'
         return ''
     args = []
     if cmmd:
-        args.append(cmmd)
+        args = cmmd
     else:
-        args.append('brilcalc')
+        args = ['brilcalc']
     args = args+['lumi']+brilcalcargs+['--output-style','csv','--without-checkjson']
     result = subprocess.check_output(args,stderr=subprocess.STDOUT)
     return '\n'.join([i for i in result.split('\n') if  len(i) > 0])
 if __name__=='__main__':
-    print brilcalcLumiHandler(['-r','284077'],cmmd='/home/zhen/work/brilws/brilcalc-run.py')
-        
+    print brilcalcLumiHandler(['-r','284077'],cmmd=['/home/zhen/work/brilws/brilcalc-run.py'])
+    #print brilcalcLumiHandler(['-r','284077'],cmmd=['/afs/cern.ch/cms/lumi/brilconda-1.1.7/bin/python','/home/data/brilws/brilcalc-run.py'])
