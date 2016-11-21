350f46bac4b10ae2f2d038a78d0c663ca61afd53
jonasdaugalas <jonasdaugalas@gmail.com>
2016-11-21 13:37:52 +0100

    Brilcalc cmmd handler return list of delivered lumi by LS

--- brilview/cmmdhandler.py
+++ brilview/cmmdhandler.py
@@ -1,24 +1,31 @@
-import os
-import sys
 import subprocess
 
-def brilcalcLumiHandler(brilcalcargs,cmmd=[]):
+
+def brilcalcLumiHandler(brilcalcargs, cmmd=[]):
     '''
-    brilcalcargs: arguments for brilcalc lumi 
+    brilcalcargs: arguments for brilcalc lumi
     cmmd : for passing brilcalc command in case you don't run standard brilcalc
     '''
-    print cmmd
     if not brilcalcargs:
         print 'about to return empty'
-        return ''
+        return []
     args = []
     if cmmd:
         args = cmmd
     else:
         args = ['brilcalc']
-    args = args+['lumi']+brilcalcargs+['--output-style','csv','--without-checkjson']
-    result = subprocess.check_output(args,stderr=subprocess.STDOUT)
-    return '\n'.join([i for i in result.split('\n') if  len(i) > 0])
-if __name__=='__main__':
-    print brilcalcLumiHandler(['-r','284077'],cmmd=['/home/zhen/work/brilws/brilcalc-run.py'])
-    #print brilcalcLumiHandler(['-r','284077'],cmmd=['/afs/cern.ch/cms/lumi/brilconda-1.1.7/bin/python','/home/data/brilws/brilcalc-run.py'])
+    args += ['lumi'] + brilcalcargs
+    args += ['--byls', '--output-style', 'csv', '--without-checkjson']
+    result = subprocess.check_output(args, stderr=subprocess.STDOUT)
+    result = [l for l in result.split('\n')
+              if len(l) > 0 and not l.startswith('#')]
+    return [float(l.split(',')[5]) for l in result]
+
+
+if __name__ == '__main__':
+    # print brilcalcLumiHandler(
+    #     ['-r', '284077'], cmmd=['/home/zhen/work/brilws/brilcalc-run.py'])
+    print brilcalcLumiHandler(
+        ['-r', '284077'],
+        cmmd=['/afs/cern.ch/cms/lumi/brilconda-1.1.7/bin/python',
+              '/home/data/brilws/brilcalc-run.py'])
