340c292c3dbcf83b0179d100df63f11369439041
jonasdaugalas <jonasdaugalas@gmail.com>
2016-11-21 13:44:17 +0100

    Fix brilcalcLumiHandler call

--- brilview.py
+++ brilview.py
@@ -1,7 +1,7 @@
 import flask
 import json
-#from handlers import brilcalc
 from brilview import cmmdhandler
+
 
 app = flask.Flask(__name__)
 
@@ -18,7 +18,10 @@ def forward_to_server():
         return ('Bad request. Query body must be not empty.', 400)
     run_from = data['from']
     run_to = data['to']
-    result = brilcalcLumiHandler(['--begin',str(run_from),'--end',str(run_to)] , cmmd=['/afs/cern.ch/cms/lumi/brilconda-1.1.7/bin/python','/home/data/brilws/brilcalc-run.py'])
+    result = cmmdhandler.brilcalcLumiHandler(
+        ['--begin', str(run_from), '--end', str(run_to)],
+        cmmd=['/afs/cern.ch/cms/lumi/brilconda-1.1.7/bin/python',
+              '/home/data/brilws/brilcalc-run.py'])
     return flask.Response(json.dumps(result), mimetype='application/json')
 
 
