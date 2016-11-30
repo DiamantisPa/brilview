2adfe55f668730eb9238491d639923e8794536f0
Zhen Xie <zhen@pczhen.cern.ch>
2016-11-30 19:53:14 +0100

    fix --daemon

--- brilview/cli/brilview_main.py
+++ brilview/cli/brilview_main.py
@@ -5,8 +5,8 @@ Options:
      --debug                                   Debug mode
      --daemon                                Daemon mode 
      --without-browser                   Run application server only
-     --config CONFIG                     Server config
-     --pidfile PIDFILE                      pid file name [default: brilview.pid]
+     --config CONFIG                      Server config
+     --pidfile PIDFILE                      pid file name 
      --host HOST                            host name [default: localhost]
      --port PORT                             port [default: 8765]  
      --user USER                            user
@@ -34,48 +34,64 @@ ch = logging.StreamHandler()
 ch.setFormatter(logformatter)
 log.addHandler(ch)
 
-def run_in_cp_tree(app, host='localhost', port=8765, daemonize=None, pidfile=None, user=None, group=None, config=None):        
+def run_in_cp_tree(app, host=None, port=None, daemonize=None, pidfile=None, user=None, group=None, config=None):        
     cp.engine.signals.subscribe()
-    cp.tree.graft(app, '')
+    cwd = os.getcwd()
     cp.config.update({
          'engine.autoreload.on': True,
-        'log.screen': True,
+         'log.screen': True,
          'server.socket_port': port,
          'server.socket_host': host
      })
-    if daemonize:
-        cp.config.update({'log.screen': False})
-        plugins.Daemonizer(cp.engine).subscribe()
-    if pidfile:
-        plugins.PIDFile(cp.engine, pidfile).subscribe()
+        
     if user or group:
-        plugins.DropPrivileges(cp.engine, uid=user, gid=group).subscribe()
+        plugins.DropPrivileges(cp.engine, uid=user, gid=group).subscribe()            
+
+    if pidfile:
+        if not os.path.isabs(pidfile):
+            print 'PID file %s must be absolute path'%pidfile
+            sys.exit(1)
+        plugins.PIDFile(cp.engine, pidfile).subscribe()
+    else:
+        if daemonize:
+            pidfile = os.path.join(cwd,'brilview.pid')
+            plugins.PIDFile(cp.engine, pidfile).subscribe()
+             
+    if daemonize:
+        cp.config.update({'log.screen': False,
+                          'log.error_file': os.path.join(cwd,'brilview.log')
+        })
+        plugins.Daemonizer(cp.engine).subscribe()
+
     if config:
         cp.config.update(config)
-    cp.engine.start()
-    cp.engine.block()
-    
+        
+    cp.tree.graft(app, script_name='/')         
+    try:
+        cp.engine.start()
+    except:
+        sys.exit(1)
+    else:
+        cp.engine.block()
+           
 def brilview_main(progname=sys.argv[0]):    
     argv = sys.argv[1:]
     parseresult = docopt.docopt(docstr,argv,help=True,version=brilview.__version__,options_first=True)
-
+    
     if parseresult['--debug']:
         log.setLevel(logging.DEBUG)
     
-    log.debug('global arguments: %s',parseresult)   
-    host =  parseresult['--host']
-    port= int(parseresult['--port'])
+    log.debug('global arguments: %s',parseresult)
+    host = parseresult['--host']
+    if not host:
+        host = 'localhost'
+    port = int(parseresult['--port'])
     if not parseresult['--daemon']:
         if not parseresult['--without-browser']:
             webbrowser.open('http://'+host+':'+str(port)+'/')
-        run_in_cp_tree(brilview_flask_app .app, host=host, port=port, daemonize=False )
-    else:
-        #if not parseresult['--config']:
-        #  print '--config option is required for daemon mode'
-        #  sys.exit(0)
-        run_in_cp_tree(brilview_flask_app.app , host=host, port=port, daemonize=True, config=parseresult['--config'])
-        pid = os.getpid()
-        print 'Process started at pid ',pid
+        run_in_cp_tree(brilview_flask_app.app, host=host, port=port, daemonize=False )
+    else:        
+        run_in_cp_tree(brilview_flask_app.app , host=host, port=port, daemonize=True,  pidfile=parseresult['--pidfile'], config=parseresult['--config'])
     return
 
     
