d899280f0a4eba9d9fdc037a9dfd8252e4850e4a
Zhen Xie <zhen@pczhen.cern.ch>
2016-12-12 15:16:49 +0100

    add globallumi and bxlumi handlers

--- brilview/handlers/cmmdhandler.py
+++ brilview/handlers/cmmdhandler.py
@@ -1,7 +1,48 @@
 import subprocess
+import re
+import numpy as np
 from brilview import common
 
-def brilcalcLumiHandler(brilcalcargs, cmmd=[]):
+def brilcalcGlobalLumiHandler(brilcalcargs,unit='/pb',cmmd=[]):
+    '''
+    brilcalcargs: arguments for brilcalc lumi without --byls
+    cmmd : for passing brilcalc command in case you don't run standard brilcalc
+    output:
+       {'status':'OK'/'ERROR', 
+         'data': [ [fillnum,runnum,tssec,delivered,recorded] ]
+             or 'errormessagestring'
+      }
+    '''
+    if not brilcalcargs:
+        return {'status':'ERROR','data':'Empty input command'}
+    args = []
+    if cmmd:
+        args = cmmd
+    else:
+        args = ['brilcalc']
+    args += ['lumi'] + brilcalcargs
+    args += ['-u', unit, '--output-style', 'csv', '--without-checkjson','--tssec']
+    r = subprocess.check_output(args, stderr=subprocess.STDOUT)
+    result_strarray = [l for l in r.split('\n') if len(l) > 0 and not l.startswith('#')]
+    if not result_strarray: #no data found
+        return { 'status':'ERROR', 'data':'No data found' }
+    resultdata = []
+    iserror = False
+    for line in result_strarray:
+        items = line.split(',')
+        if items[0].find(':') == -1: #output is an error message
+            iserror = True
+            break
+        [runnum,fillnum] = [ int(x) for x in items[0].split(':') ]
+        tssec =  int(items[1])
+        delivered = float(items[4])
+        recorded = float(items[5])
+        resultdata.append( [fillnum,runnum,tssec,delivered,recorded] )
+    if iserror:
+        return {'status':'ERROR', 'data': '\n'.join( result_strarray ) }
+    return { 'status':'OK', 'data': resultdata}
+        
+def brilcalcLumiHandler(brilcalcargs, unit='/ub',cmmd=[]):
     '''
     brilcalcargs: arguments for brilcalc lumi
     cmmd : for passing brilcalc command in case you don't run standard brilcalc
@@ -19,7 +60,7 @@ def brilcalcLumiHandler(brilcalcargs, cmmd=[]):
     else:
         args = ['brilcalc']
     args += ['lumi'] + brilcalcargs
-    args += ['--byls', '--output-style', 'csv', '--without-checkjson','--tssec']
+    args += ['-u', unit, '--byls', '--output-style', 'csv', '--without-checkjson','--tssec']
     r = subprocess.check_output(args, stderr=subprocess.STDOUT)
     result_strarray = [l for l in r.split('\n') if len(l) > 0 and not l.startswith('#')]
     if not result_strarray: #no data found
@@ -43,11 +84,61 @@ def brilcalcLumiHandler(brilcalcargs, cmmd=[]):
         return {'status':'ERROR', 'data': '\n'.join( result_strarray ) }
     return { 'status':'OK', 'data': resultdata}
 
+def brilcalcBXLumiHandler(brilcalcargs, unit='/ub',cmmd=[]):
+    '''
+    output:
+       {'status':'OK'/'ERROR', 
+        'data': [ [fillnum,runnum,lsnum,tssec,bxid_array,bxdelivered_array,bxrecorded_array] ]
+        or 'errormessagestring'
+    }
+    '''
+    if not brilcalcargs:
+        return {'status':'ERROR','data':'Empty input command'}
+    args = []
+    if cmmd:
+        args = cmmd
+    else:
+        args = ['brilcalc']
+    xingMin = 0.001
+    args += ['lumi'] + brilcalcargs
+    args += ['-u', unit, '--xing', '--output-style', 'csv', '--without-checkjson','--tssec','--xingMin',str(xingMin)]
+    r = subprocess.check_output(args, stderr=subprocess.STDOUT)
+    result_strarray = [l for l in r.split('\n') if len(l) > 0 and not l.startswith('#')]
+    if not result_strarray: #no data found
+        return { 'status':'ERROR', 'data':'No data found' }
+    resultdata = []
+    iserror = False
+    for line in result_strarray:
+        items = line.split(',')
+        if items[0].find(':') == -1: #output is an error message
+            iserror = True
+            break
+        [runnum,fillnum] = [ int(x) for x in items[0].split(':') ]
+        [lsnum,cmsalive] = [ int(x) for x in items[1].split(':') ]
+        tssec =  int(items[2])
+        bxlumi_str = items[9]
+        bxlumi_str = re.sub(r'\[|\]','',bxlumi_str)        
+        bxlumi = np.fromstring(bxlumi_str,dtype='f4', sep=' ')
+        bxid_array = bxlumi[0::3].astype(int)
+        bxdelivered_array = bxlumi[1::3]
+        bxrecorded_array =  bxlumi[2::3]
+        resultdata.append( [fillnum, runnum,lsnum,tssec,bxid_array.tolist(),bxdelivered_array.tolist(),bxrecorded_array.tolist() ] )
+    if iserror:
+        return {'status':'ERROR', 'data': '\n'.join( result_strarray ) }
+    return { 'status':'OK', 'data': resultdata}
 
+def brilcalcBeamHandler(brilcalcargs, cmmd=[]):
+    pass
+
+def brilcalcBXBeamHandler(brilcalcargs, cmmd=[]):
+    pass
+    
 if __name__ == '__main__':
      #from project root run python -m brilview.handlers.cmmdhandler
      print brilcalcLumiHandler(
-         ['-r', '284077'], cmmd=['/home/zhen/work/brilws/brilcalc-run.py'])
+         ['-r', '284077'], unit='/mb',cmmd=['/home/zhen/work/brilws/brilcalc-run.py'])
+     print brilcalcBXLumiHandler(
+         ['-r', '284077'], unit='/mb',cmmd=['/home/zhen/work/brilws/brilcalc-run.py'])
     #print brilcalcLumiHandler(
     #    ['-r', '284077'],
     #    cmmd=['/home/data/brilws/brilcalc-run.py'])
