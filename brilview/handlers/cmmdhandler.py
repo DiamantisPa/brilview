00316d782294d0042e7373f8704db9e685073d58
Zhen Xie <zhen@pczhen.cern.ch>
2016-12-14 19:22:59 +0100

    unify lumihandler

--- brilview/handlers/cmmdhandler.py
+++ brilview/handlers/cmmdhandler.py
@@ -2,87 +2,120 @@ import subprocess
 import re
 import numpy as np
 from brilview import common
+from brilview import config
 
-def brilcalcGlobalLumiHandler(brilcalcargs,unit='/pb',cmmd=[]):
-    '''
-    brilcalcargs: arguments for brilcalc lumi without --byls
-    cmmd : for passing brilcalc command in case you don't run standard brilcalc
-    output:
-       {'status':'OK'/'ERROR', 
-         'data': [ [fillnum,runnum,tssec,delivered,recorded] ]
-             or 'errormessagestring'
-      }
-    '''
-    if not brilcalcargs:
-        return {'status':'ERROR','data':'Empty input command'}
-    args = []
-    if cmmd:
-        args = cmmd
-    else:
-        args = ['brilcalc']
-    args += ['lumi'] + brilcalcargs
-    args += ['-u', unit, '--output-style', 'csv', '--without-checkjson','--tssec']
-    r = subprocess.check_output(args, stderr=subprocess.STDOUT)
-    result_strarray = [l for l in r.split('\n') if len(l) > 0 and not l.startswith('#')]
-    if not result_strarray: #no data found
-        return { 'status':'ERROR', 'data':'No data found' }
-    resultdata = []
-    iserror = False
-    for line in result_strarray:
-        items = line.split(',')
-        if items[0].find(':') == -1: #output is an error message
-            iserror = True
-            break
-        [runnum,fillnum] = [ int(x) for x in items[0].split(':') ]
-        tssec =  int(items[1])
-        delivered = float(items[4])
-        recorded = float(items[5])
-        resultdata.append( [fillnum,runnum,tssec,delivered,recorded] )
-    if iserror:
-        return {'status':'ERROR', 'data': '\n'.join( result_strarray ) }
-    return { 'status':'OK', 'data': resultdata}
-        
-def brilcalcLumiHandler(brilcalcargs, unit='/ub',cmmd=[]):
+def brilcalcLumiHandler(commandargs={}):
     '''
     brilcalcargs: arguments for brilcalc lumi
-    cmmd : for passing brilcalc command in case you don't run standard brilcalc
+    
+    the handler decides if --byls is needed according to input time parameters
+    input:
+         commandargs: arguments for command 
+         {
+           begin: str '^\d\d/\d\d/\d\d \d\d:\d\d:\d\d$|^\d{6}$|^\d{4}$'
+           end: str '^\d\d/\d\d/\d\d \d\d:\d\d:\d\d$|^\d{6}$|^\d{4}$'
+           timeunit: run,fill,strdate
+           unit: str 
+           beamstatus: str (must be in 'stable beams','squeeze','flat top','adjust')
+           normtag: str textbox or dropdown select
+           datatag: str
+           hltpath:  str //'^HLT_[\w\*\?\[\]\!]+$' reject wildcard patterns ?? 
+           type:  str or None
+           selectjson: jsonstr
+           byls: boolean
+         }
+
     output:
-      {'status':'OK'/'ERROR', 
-       'data': [ [fillnum,runnum,lsnum,tssec,cmsalive,beamstatusid,delivered,recorded] ]
-             or 'errormessagestring'
-      }
+    {'status':'OK'/'ERROR', 
+    'data': {'fillnum': [], 
+                  'runnum': [],
+                  'tssec': [],
+                  'delivered': [],
+                   'recorded':[], 
+                    'lsnum':[] or None }
+    or {'message':str}
+    }
+
     '''
-    if not brilcalcargs:
-        return {'status':'ERROR','data':'Empty input command'}
-    args = []
-    if cmmd:
-        args = cmmd
+    begin = ''
+    end = ''
+    selectjson = ''
+        
+    cmd = 'brilcalc'
+    if config.appconfig.has_key('brilcommandhandler') and config.appconfig['brilcommandhandler'].has_key('command') and config.appconfig['brilcommandhandler']['command'] :
+        cmd = config.appconfig['brilcommandhandler']['command']
+    cmdargs = [cmd,'lumi','--output-style', 'csv', '--without-checkjson','--tssec']    
+    if not commandargs.has_key('selectjson') :
+        if not commandargs.has_key('begin') or not commandargs.has_key('end')  :
+            return 'ERROR: parameters begin, end or selectjson are not provided'
+        else:
+            begin = commandargs['begin']
+            end = commandargs['end']
+            cmdargs += ['--begin',str(begin),'--end',str(end)]
     else:
-        args = ['brilcalc']
-    args += ['lumi'] + brilcalcargs
-    args += ['-u', unit, '--byls', '--output-style', 'csv', '--without-checkjson','--tssec']
-    r = subprocess.check_output(args, stderr=subprocess.STDOUT)
+        selectjson = commandargs['selectjson']
+        cmdargs += ['-i',selectjson]
+        
+    byls = False
+    if commandargs.has_key('byls') and commandargs['byls'] is True:
+        byls = True
+        cmdargs.append('--byls')
+        
+    unit = '/ub'
+    if commandargs.has_key('unit') and commandargs['unit'] :
+        unit = commandargs['unit']
+    cmdargs += ['-u',unit]
+        
+    if commandargs.has_key('type') and commandargs['type']:
+        cmdargs += [ '--type', commandargs['type'] ]
+        
+    if commandargs.has_key('beamstatus') and commandargs['beamstatus']:
+        cmdargs += [ '-b', commandargs['beamstatus'].upper() ]
+        
+    if commandargs.has_key('hltpath') and commandargs['hltpath']:
+        cmdargs += [ '--hltpath', commandargs['hltpath'] ]
+    print cmdargs
+    r = subprocess.check_output(cmdargs, stderr=subprocess.STDOUT)
     result_strarray = [l for l in r.split('\n') if len(l) > 0 and not l.startswith('#')]
     if not result_strarray: #no data found
-        return { 'status':'ERROR', 'data':'No data found' }
-    resultdata = []
+        return { 'status':'ERROR', 'message':'No data found' }
+    print cmdargs
     iserror = False
+    fillnums = []
+    runnums = []
+    lsnums = []
+    tssecs = []
+    delivereds = []
+    recordeds = []
+    
     for line in result_strarray:
         items = line.split(',')
         if items[0].find(':') == -1: #output is an error message
             iserror = True
             break
+        print items[0]
         [runnum,fillnum] = [ int(x) for x in items[0].split(':') ]
-        [lsnum,cmsalive] = [ int(x) for x in items[1].split(':') ]
-        tssec =  int(items[2])
-        beamstatus = items[3]
-        beamstatusid = common.beamstatustoid[beamstatus]
-        delivered = float(items[5])
-        recorded = float(items[6])
-        resultdata.append( [fillnum,runnum,lsnum,tssec,cmsalive,beamstatusid,delivered,recorded] )
+
+        fillnums.append(fillnum)
+        runnums.append(runnum)
+        
+        if byls:
+            tssecs.append( int(items[2]) )
+            delivereds.append( float(items[5]) )
+            recordeds.append( float(items[6]) )
+            lsnum = [ int(x[0]) for x in items[1].split(':') ]
+            lsnums.append(lsnum)
+        else:
+            tssecs.append( int(items[1]) )
+            delivereds.append( float(items[4]) )
+            recordeds.append( float(items[5]) )
+            
     if iserror:
-        return {'status':'ERROR', 'data': '\n'.join( result_strarray ) }
+        return {'status':'ERROR', 'message': '\n'.join( result_strarray ) }
+    
+    resultdata = {'fillnum':fillnums, 'runnum':runnums, 'lsnum':lsnums ,'tssec':tssecs, 'delivered':delivereds,'recorded':recordeds}
     return { 'status':'OK', 'data': resultdata}
+
 
 def brilcalcBXLumiHandler(brilcalcargs, unit='/ub',cmmd=[]):
     '''
@@ -135,10 +168,13 @@ def brilcalcBXBeamHandler(brilcalcargs, cmmd=[]):
     
 if __name__ == '__main__':
      #from project root run python -m brilview.handlers.cmmdhandler
-     print brilcalcLumiHandler(
-         ['-r', '284077'], unit='/mb',cmmd=['/home/zhen/work/brilws/brilcalc-run.py'])
-     print brilcalcBXLumiHandler(
-         ['-r', '284077'], unit='/mb',cmmd=['/home/zhen/work/brilws/brilcalc-run.py'])
-    #print brilcalcLumiHandler(
-    #    ['-r', '284077'],
-    #    cmmd=['/home/data/brilws/brilcalc-run.py'])
+     #print brilcalcLumiHandler(
+     #    ['-r', '284077'], unit='/mb',cmmd=['/home/zhen/work/brilws/brilcalc-run.py'])
+     #print brilcalcBXLumiHandler(
+     #    ['-r', '284077'], unit='/mb',cmmd=['/home/zhen/work/brilws/brilcalc-run.py'])
+     #print brilcalcLumiHandler(
+     #    ['-r', '284077'],
+     #    cmmd=['/home/data/brilws/brilcalc-run.py'])
+     config.appconfig['brilcommandhandler'] = {}
+     config.appconfig['brilcommandhandler']['command'] = '/home/zhen/work/brilws/brilcalc-run.py'
+     print brilcalcLumiHandler({'begin':284077,'end':284077})
