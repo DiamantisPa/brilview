599da074fd34f5d60c964253f89044bf5f42d82f
jonasdaugalas <jonasdaugalas@gmail.com>
2016-12-14 16:47:21 +0100

    Add Clarity styles and Plotly.js

--- web/script.js
+++ web/script.js
@@ -2,63 +2,75 @@ var app = angular.module('app', []);
 
 app.controller("TestController", function($http, $timeout) {
     var me = this;
-    this.run_from = 0;
-    this.run_to = 0;
+    this.time_from = null;
+    this.time_to = null;
+    this.time_unit = "RUN";
+    this.lumi_type = null;
+    this.normtag = null;
+    this.byls = true;
+    this.beam_status = null;
     this.status = "";
     this.message = "";
 
-    var chart = null;
-    var options = {
-        chart: {
-            renderTo: "chart1"
-        },
-        xAxis: {
-            type: "datetime"
-        },
-        series: [{
-            name: 'field 6',
-            data: [7.0, 6.9, 9.5]
-        }, {
-            name: 'field 7',
-            data: [14.5, 18.2, 21.5]
-        }]
+    var chartData = [{
+        x: [1, 2, 3, 4],
+        y: [16, 5, 11, 9],
+        mode: "lines"
+    }, {
+        x: [1, 2, 3, 4],
+        y: [10, 15, 13, 17],
+        mode: "lines"
+    }];
+    var chartLayout = {
+        xaxis: {
+            type: "date"
+        }
+    };
+    var chartConfig = {
+        displaylogo: false,
+        showTips: false,
+        modeBarButtonsToRemove: ["sendDataToCloud", "lasso2d"]
     };
 
     $timeout(function() {
-        chart = new Highcharts.Chart(options);
+        Plotly.newPlot('chart1', chartData, chartLayout, chartConfig);
+        // chart = new Highcharts.Chart(options);
     });
 
     this.update_chart = function() {
         var body = {
-            "from": me.run_from,
-            "to": me.run_to};
+            "from": me.time_from,
+            "to": me.time_to,
+            "time_unit": me.time_unit};
         console.log(body);
+
         $http.post("/api/query", body)
             .then(function(response) {
-                var chartData, data, i;
+                var i, x, y1, y2, data;
                 console.log(response);
                 me.status = response.data.status;
                 if (me.status === "OK") {
                     me.message = "";
                     data = response.data.data;
-                    chartData = [];
+                    x = [];
+                    y1 = [];
+                    y2 = [];
                     for (i = 0; i < data.length; i++) {
-                        chartData.push([data[i][3], data[i][6]]);
+                        x.push(data[i][3] * 1000);
+                        y1.push(data[i][6]);
+                        y2.push(data[i][7]);
                     }
-                    console.log(data);
-                    console.log(chartData);
-                    chart.series[0].setData(chartData);
-                    chartData = [];
-                    for (i = 0; i < data.length; i++) {
-                        chartData.push([data[i][3], data[i][7]]);
-                    }
-                    console.log(chartData);
-                    chart.series[1].setData(chartData);
+                    chartData[0].x = x;
+                    chartData[0].y = y1;
+                    chartData[1].x = x;
+                    chartData[1].y = y2;
+                    Plotly.redraw('chart1');
                 } else {
                     me.message = response.data.data;
                 }
             }, function(response) {
                 me.status = "HTTP failed";
+                me.message = response.data;
                 console.error(response);
             });
     };
