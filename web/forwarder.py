b0ad24c586471863c24f439451f19342c870281b
jonasdaugalas <jonasdaugalas@gmail.com>
2016-12-16 18:15:54 +0100

    Return exceptions as HTTP body

--- web/forwarder.py
+++ web/forwarder.py
@@ -17,24 +17,19 @@ def api():
 
 @app.route(BASE + '/query', methods=['GET', 'POST'])
 def forward_to_server():
-    data = flask.request.json
-    if data is None:
-        return ('Bad request. Body is empty?', 400)
-    req = urllib2.Request(
-        REMOTE_API + '/query',
-        json.dumps(data),
-        {'Content-Type': 'application/json'})
-    response = urllib2.urlopen(req)
-    result = response.read()
-    return flask.Response(result, mimetype='application/json')
-
-
-@app.route(BASE + '/vocms-test')
-def vocms_test():
-    request = urllib2.urlopen('http://' + REMOTE_HOST)
-    data = request.read()
-    request.close()
-    return data
+    try:
+        data = flask.request.json
+        if data is None:
+            return ('Bad request. Body is empty?', 400)
+        req = urllib2.Request(
+            REMOTE_API + '/query',
+            json.dumps(data),
+            {'Content-Type': 'application/json'})
+        response = urllib2.urlopen(req)
+        result = response.read()
+        return flask.Response(result, mimetype='application/json')
+    except Exception as e:
+        return (str(e), 500)
 
 
 if __name__ == '__main__':
