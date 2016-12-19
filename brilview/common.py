440b47ccb2004de95da42c8ee5e3d07130bb0694
Zhen Xie <zhen@pczhen.cern.ch>
2016-12-19 19:21:28 +0100

    use dictionary input/output

--- brilview/common.py
+++ brilview/common.py
@@ -1,18 +1,17 @@
 import ConfigParser
 import numpy as np
+import numbers
 
-beamstatustoid = {'NO BEAM':0,'SETUP':1,'ABORT':2,'INJECTION PROBE BEAM':3,'INJECTION SETUP BEAM':4,'INJECTION PHYSICS BEAM':5,'PREPARE RAMP':6,'RAMP':7,'FLAT TOP':8,'SQUEEZE':9,'ADJUST':10,'STABLE BEAMS':11,'UNSTABLE BEAMS':12,'BEAM DUMP WARNING':13,'BEAM DUMP':14,'RAMP DOWN':15,'CYCLING':16,'RECOVERY':17,'INJECT & DUMP':18,'CIRCULATE & DUMP':19,'UNKNOWN':20}
-
-class ini_parser(ConfigParser.SafeConfigParser):
-    '''
-    extended SafeConfigParser class adding as_dict method
-    '''
-    def as_dict(self):
-        d = dict(self._sections)
-        for k in d:
-            d[k] = dict(self._defaults, **d[k])
-            d[k].pop('__name__', None)
-        return d
+#class ini_parser(ConfigParser.SafeConfigParser):
+#    '''
+#    extended SafeConfigParser class adding as_dict method
+#    '''
+#    def as_dict(self):
+#        d = dict(self._sections)
+#        for k in d:
+#            d[k] = dict(self._defaults, **d[k])
+#            d[k].pop('__name__', None)
+#        return d
 
 def blob_to_array(iblob,itypecode):
     '''
@@ -29,54 +28,102 @@ def blob_to_array(iblob,itypecode):
     result.fromstring(iblob)
     return result
 
-def groupby_1d(bindef, binned_array, groupby_array, groupbyalgos ):
+def smart_groupby_1d(data, bindef, aggalgos):
     '''
-    aggregate array data by bins
+    inputs:
+      data: {
+        'time':...
+        'val1': ...
+     }
+     bindef: {
+        field: str,
+        step_size: int | None,
+        step_unit: str (number,week,month,day) | None,
+        step_count: int |None,        
+     }
+     aggalgos: {
+           "<fieldname>": str, # left, right, middle, min, max, avg, sum
+           ...
+     }
 
-    Input parameters:
-        bindef: grouping boundaries
-        binned_array: the array to be divided into bins
-        groupbyalgos: aggregation algorithm of the binned values ['average','sum','max','min']
-        groupby_array: array to be aggregated according to the binning
+    outputs:
+    {
+         'aggregated': True/False
+         'data':{
+             field: array
+        }
+    }
+    '''
+    if not bindef.has_key('field'):
+        raise KeyError('field not found in the bindef input, cannot decide on the binning variable')
+    
+    binning_field = bindef['field']    
+    binning_array = data[binning_field]
+    step_count = 0
+    step_size = 0
+    bins = 100 #well, default to 100 bins if nothing given
+    if bindef.has_key('step_count') and bindef['step_count']:
+        bins = bindef['step_count']
+    elif not bins and bindef.has_key('step_size') and  bindef['step_size']:
+        bins =  np.arange( min(binning_array), max(binning_array), bindef['step_size'] )
+
+    if isinstance(bins, numbers.Number):
+        if bins > len(binning_array): # no need of aggregation
+            return { 'aggregated':False }
+    else:
+        if bins.size > len(binning_array): # no need of aggregation
+            return { 'aggregated':False }
+    (hist, bin_edges) = np.histogram( np.array(binning_array), bins=bins,  )
+
+    outdata = dict.fromkeys(aggalgos.keys(),[])
+    if not outdata.has_key(binning_field):
+        outdata[ binning_field ] = []
+
+    for i,e in enumerate( bin_edges[:-1] ):
+        left_edge = e
+        right_edge = bin_edges[i+1]        
+        if i==(bin_edges.size-2):#if last bin, the right edge is inclusive            
+            pos = np.where( (binning_array<=right_edge)&(binning_array>=left_edge) )[0]
+        else:
+            pos = np.where( (binning_array<right_edge)&(binning_array>=left_edge) )[0]
         
-        binned_array and groupby_array must have the same length
+        outdata[ binning_field ].append( (left_edge+right_edge)/2. )
+        for fieldname in aggalgos.keys() :
+            aggalgo = aggalgos[fieldname]
+            agg_array = data[fieldname]
+            toaggregate = np.take(agg_array, pos)
+            if len(toaggregate)==0:
+                outdata[fieldname].append(None) # or zero???
+                continue            
+            if aggalgo=='average':
+                outdata[fieldname].append( np.average(toaggregate) )
+            elif aggalgo=='sum':
+                outdata[fieldname].append( np.sum(toaggregate) )
+            elif aggalgo=='max':
+                outdata[fieldname].append( np.max(toaggregate) )
+            elif aggalgo=='min':
+                outdata[fieldname].append( np.min(toaggregate) )
+            elif aggalgo=='left':
+                outdata[fieldname].append( toaggregate[0] )
+            elif aggalgo=='right':
+                outdata[fieldname].append( toaggregate[-1] )
+            elif aggalgo=='middle':
+                if len(toaggregate)==1:
+                    outdata[fieldname].append( toaggregate[0] )
+                elif len(toaggregate)%2==0: #even number of elements
+                    outdata[fieldname].append( toaggregate[ len(toaggregate)/2-1 ] )
+                else:    
+                    outdata[fieldname].append( toaggregate[ len(toaggregate)/2 ] )
+    
+    return {'aggregated':True, 'data':outdata}
 
-    Output parameters:
-        groupresult = {'bin':index of the left-most element in each bin in the input array , 'algo1': resultarray}
-        The result arrays all have the same length 
-    '''
-    binindx = np.digitize( binned_array, bindef, right=False )
-    bins, indices = np.unique(binindx, return_index=True) # pandas unique is faster?
-    #The indices of the first occurrences of the unique values
-    result = {}
-    result['binidx'] = indices
-    result['bin'] = np.take(binned_array,indices)
-    for k in bins: #loop over bins in order        
-        toaggregate_index = np.where(binindx==k)
-        toaggregate = np.take(groupby_array,  toaggregate_index)
-        for algo in groupbyalgos:
-            if not result.has_key(algo):
-                result[algo] = []
-            if algo=='average':
-                result[algo].append( np.average(toaggregate) )
-            elif algo=='sum':
-                result[algo].append( np.sum(toaggregate) )
-            elif algo=='max':
-                result[algo].append( np.amax(toaggregate) )
-            elif algo=='min':
-                result[algo].append( np.amin(toaggregate) )
-            elif  algo=='cumsum':
-                result[algo].append( np.cumsum(toaggregate) )
-    return result
+if __name__=='__main__':    
 
-if __name__=='__main__':
-    iconf = ini_parser()
-    iconf.read('/home/zhen/work/brilview/brilview/data/brilview.config')
-    print  iconf.sections()
-    print iconf.as_dict()
-
-    bindef = [0., 1., 2.5, 4.0, 10.0]
-    t = [0.2, 0.3, 0.4, 1.1, 1.2, 3.3, 3.0, 4.1, 6.2, 16.]
+    t = [0.2, 0.3, 0.4, 1.1, 1.2, 3.3, 3.0, 4.1, 6.2, 16.1]
     y = [1,2,3,4,5,6,7,8,9,10]
-    grouped = groupby_1d(bindef, t, y, ['min','max','average'] )
-    print grouped
+
+    data = {'t':t,'y':y}
+    bindefdict = {'field':'t','step_count':5 }
+    aggalgos = {'y':'max'}
+    smart_grouped =  smart_groupby_1d(data, bindefdict, aggalgos)
+    print smart_grouped
