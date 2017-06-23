ac57c932e0020ac6a5b04d587b5fa769724b5d12
jonasdaugalas <jonasdaugalas@gmail.com>
2017-06-23 13:54:08 +0200

    Work on 'in view' stats

--- brilview/web/src/app/lumi-inspector/stats/stats.component.ts
+++ brilview/web/src/app/lumi-inspector/stats/stats.component.ts
@@ -12,6 +12,10 @@ export class StatsComponent implements OnInit {
 
     @Input('chart') chart;
     stats = [];
+    visibleRanges = {
+        x: [-Infinity, Infinity],
+        y: [-Infinity, Infinity]
+    };
 
     constructor() { }
 
@@ -22,11 +26,51 @@ export class StatsComponent implements OnInit {
         return this.chart.getNativeChartElement().data;
     }
 
+    protected getVisibleRanges() {
+        const layout = this.chart.getNativeChartElement().layout;
+        // x - array of two Date objects or infinities. If xrange from chart
+        // comes as two strings, then they need to be treated as UTC
+        return {
+            x: (layout.xaxis && layout.xaxis.range
+                ? layout.xaxis.range
+                : [-Infinity, Infinity]
+               ).map(x => {
+                   return typeof x === 'string' && !x.endsWith('Z')
+                       ? new Date(x + 'Z')
+                       : x;
+               }),
+            y: layout.yaxis && layout.yaxis.range
+                ? layout.yaxis.range.slice()
+                : [-Infinity, Infinity]
+        }
+    }
+
+    toISOStringNoThrow(date) {
+        try {
+            return date.toISOString();
+        } catch (any){
+            return date;
+        }
+    }
+
     recalculate() {
         const series = this.getSeries();
+        const visibleRanges = this.getVisibleRanges();
+        console.log(visibleRanges);
+        const visible = series.map(s => {
+            return {
+                name: s.name + ' * in view',
+                y: s.y.filter((y, idx) => {
+                    return (new Date(s.x[idx]) >= visibleRanges.x[0] &&
+                            new Date(s.x[idx]) <= visibleRanges.x[1]);
+                }).filter(y => {
+                    return y >= visibleRanges.y[0] && y <= visibleRanges.y[1];
+                })
+            };
+        });
         const new_stats = [];
         let values = null;
-        for (const s of series) {
+        for (const s of series.concat(visible)) {
             values = s.y.filter(Number.isFinite);
             new_stats.push({
                 name: s.name,
@@ -39,5 +83,6 @@ export class StatsComponent implements OnInit {
             });
         }
         this.stats = new_stats;
+        this.visibleRanges = visibleRanges;
     }
 }
