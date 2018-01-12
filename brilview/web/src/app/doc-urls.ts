1c5e9f0b95952429ab117689bf5acffa68db5e0b
jonas <jonas@jdhost.cern.ch>
2018-01-12 19:47:57 +0100

    Update documentation urls

--- brilview/web/src/app/doc-urls.ts
+++ brilview/web/src/app/doc-urls.ts
@@ -1,7 +1,15 @@
 export function docURLFromRouteURL(routeurl) {
     switch (routeurl) {
+    case '/home':
+        return 'http://brilview.readthedocs.io/en/latest/index.html';
+    case '/totlumi/live-bestlumi':
+        return 'http://brilview.readthedocs.io/en/latest/bestlumi_live.html';
     case '/totlumi':
-        return 'http://brilview.readthedocs.io/en/latest/lumi_inspector.html';
+        return 'http://brilview.readthedocs.io/en/latest/total_lumi_inspector.html';
+    case '/bxlumi':
+        return 'http://brilview.readthedocs.io/en/latest/bx_lumi_inspector.html';
+    case '/totlumi/atlaslumi':
+        return 'http://brilview.readthedocs.io/en/latest/atlas_lumi.html';
     default:
         return 'http://brilview.readthedocs.io';
     }
