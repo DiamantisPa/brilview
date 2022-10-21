3d29999f29ffa5aaf5704c966c1cf2dcc0850f99
Diamantis Patsidis <diamantis.patsidis@cern.ch>
2022-10-21 12:55:45 +0200

    1.5.4

--- brilview/web/src/app/app.config.ts
+++ brilview/web/src/app/app.config.ts
@@ -1,5 +1,5 @@
 export const LUMI_TYPES = [
-    'Online', 'PLTZERO', 'HFOC', 'HFET', 'BCM1F', 'PXL', 'DT', 'RAMSES', 'BCM1FUTCA'
+    'Online', 'PLTZERO', 'HFOC', 'HFET', 'BCM1F', 'PXL', 'DT', 'RAMSES', 'BCM1FUT'
 ];
 
 export const BEAMS = ['STABLE BEAMS', 'ADJUST', 'SQUEEZE', 'FLAT TOP'];
