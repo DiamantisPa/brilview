7568a5bdab40992f7aacdbd9c46338cc9a9784df
DiamantisPa <diamantispa@gmail.com>
2023-07-11 15:21:54 +0200

    update storage

--- brilview/web/src/app/lumi-inspector/atlaslumi-data.service.ts
+++ brilview/web/src/app/lumi-inspector/atlaslumi-data.service.ts
@@ -63,6 +63,9 @@ export class AtlaslumiDataService {
         const id = this.lumiDataService.lastStorageID = this.lumiDataService.lastStorageID + 1;
         const name = this.makeLumiDataName(params, data);
         console.log('name ', name);
+        for (let i = 1; i < data['timestamp'].length; i++) {
+            data['single_fillnum'].push(data['single_fillnum']);
+        }
         if (this.lumiDataService.storage.hasOwnProperty(id)) {
             throw Error('Cannot insert lumi data. ID already exists.');
         }
